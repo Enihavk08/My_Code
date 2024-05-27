a90b8cbf9ba469e3617f74cbc49becdb89511c20
Enihavk08 <132152067+Enihavk08@users.noreply.github.com>
2024-05-27 14:59:58 +0530

    Create Property.js

---
+++ backend/config/backend/models/Property.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const PropertySchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  address: { type: String, required: true },
+  price: { type: Number, required: true },
+  owner: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Property', PropertySchema);
